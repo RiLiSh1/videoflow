09f978e6ee83754c3a48c6f81ffbb8159d3af1fa
瀧本 琢真 <takimoto@takimotonoMacBook-Pro.local>
2026-02-27 13:26:54 +0900

    auto: 2026-02-27 13:26:54

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -70,6 +70,7 @@ export async function POST(request: Request) {
         email: true,
         role: true,
         chatworkId: true,
+        chatworkRoomId: true,
         isActive: true,
         createdAt: true,
       },
